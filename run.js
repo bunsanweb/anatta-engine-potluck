dfc13e4df3f540445d2498df0933e8a078ac39da
bellbind <bellbind@gmail.com>
2013-03-11 17:47:01 +0900

    [update] initial schema of application
    	modified:   run.js
    	new file:   src/posts.html
    	new file:   ui/form.html
    	new file:   ui/index.html

--- run.js
+++ run.js
@@ -1,1 +1,23 @@
 var anatta = require("anatta-engine");
+
+var engine = anatta.engine.builder.engine({
+    type: "generic",
+    porter: {
+        "text/html": "html",
+        "application/json": "json",
+    },
+    space: {
+        "orb:/": {field: "orb"},
+        "src:/": {field: "file", root: "./src/", prefix: "/"},
+        "root:/": {field: "file", root: "./ui/", prefix: "/"},
+        "root:/posts/": {field: "agent", uri: "src:/posts.html"},
+        "root:/formpost/": {field: "agent", uri: "src:/formpost.html"},
+        "root:/list/": {field: "agent", uri: "src:/list.html"},
+        "root:/link/": {field: "agent", uri: "src:/link.html"},
+    },
+});
+//var uri = "mongodb://localhost/potluck";
+//engine.space.maanager.fields["orb|orb:/"].orb = anatta.orb.mondodb.Orb(uri);
+var gate = anatta.webgate.core.WebGate(
+    engine.space, {from: "/", to: "root:/"});
+gate.start(process.argv[2] || process.env.PORT || "8000");
