a3b426d2afe623fae85d52c670954451b8d1414a
bellbind <bellbind@gmail.com>
2013-03-11 18:10:05 +0900

    [update] initial post form
    	modified:   ui/form.html
    	new file:   ui/form.js

---
+++ ui/form.js
@@ -0,0 +1,21 @@
+"use strict";
+
+window.addEventListener("load", function (ev) {
+    var doPost = function (ev) {
+        var postUri = "/formpost/";
+        var form = new FormData();
+        form.append("url", document.getElementById("url").value);
+        form.append("title", document.getElementById("title").value);
+        form.append("tags", document.getElementById("tags").value);
+        form.append("comment", document.getElementById("comment").value);
+        var req = new XMLHttpRequest();
+        req.addEventListener("load", onLoad.bind(req), false);
+        req.open("POST", postUri, true);
+        req.send(form);
+    };
+    var onLoad = function (ev) {
+        window.location.href = "/";
+    };
+    
+    document.getElementById("post").addEventListener("click", doPost, false);
+}, false);
