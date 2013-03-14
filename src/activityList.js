566bed9b5b575f9d7bd37a97c31cd44e31f67f01
bellbind <bellbind@gmail.com>
2013-03-14 17:24:49 +0900

    support to get src/activities generated contents
    	modified:   src/activityList.js
    	modified:   ui/activitiesIndex.js
    	modified:   ui/index.html

--- src/activityList.js
+++ src/activityList.js
@@ -13,10 +13,19 @@ window.addEventListener("agent-load", function (ev) {
         default: return ev.detail.respond("405", {allow: "GET"}, "");
         }
     }, false);
-
+    
+    var activity = function (id) {
+        var uri = anatta.builtin.url.resolve(base, id);
+        return anatta.engine.link({href: uri});
+    };
+    
     var get = function (ev) {
-        console.log("get raw activity index in orb");
-        return index.get().then(function (entity) {
+        //console.log("get raw activity index in orb");
+        var id = ev.detail.request.location.query.id;
+        var page = id ? activity(id) : index;
+        return page.get().then(function (entity) {
+            //console.log(entity.response.status);
+            //console.log(entity.response.text());
             ev.detail.respond(
                 entity.response.status,
                 entity.response.headers,
--- ui/activitiesIndex.js
+++ ui/activitiesIndex.js
@@ -5,14 +5,20 @@
 window.addEventListener("load", function (ev) {
     var main = document.querySelector("main");
     var indexUri = "/activityList/";
-    var query = "#index";
+    var query = "#links";
     
     var req = new XMLHttpRequest();
     req.addEventListener("load", function (ev) {
         var doc = document.implementation.createHTMLDocument("");
-        doc.documentElement.innerHTML = ev.responseText;
+        doc.documentElement.innerHTML = this.responseText;
         var listPart = doc.querySelector(query);
-        if (listPart) main.innerHTML = listPart.innerHTML;
+        if (listPart) {
+            main.innerHTML = listPart.innerHTML;
+            var linkedList = main.querySelectorAll("[href]");
+            Array.prototype.forEach.call(linkedList, function (link) {
+                link.href = indexUri + "?id=" + link.getAttribute("href");
+            });
+        }
     }, false);
     req.open("GET", indexUri, true);
     req.send();
