bbd0619b878bd1a30781c91a560a773a24e5a8e9
bellbind <bellbind@gmail.com>
2016-02-22 17:32:40 +0900

    [update] modernized author.js agent (not yet implemented?)

--- src/author.js
+++ src/author.js
@@ -1,6 +1,6 @@
 "use strict";
-window.addEventListener("agent-load", function (ev) {
-    window.addEventListener("agent-access", function (ev) {
+window.addEventListener("agent-load", ev => {
+    window.addEventListener("agent-access", ev => {
         ev.detail.accept();
         ev.detail.respond("200", {
             "content-type": "text/html;charset=utf-8"
